952c07527f742fc0c7ff50b529c842f6a1a3d721
mariah622 <mjustine622@gmail.com>
2022-01-26 20:16:18 -0500

    commit tree 65b5d79b91991036c0a0d4d28beb72de7aeb9695

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-// Write your solution in this file!
-var customerName = 'bob'
-const leastFavoriteCustomer = 'bob is that you?'
+// // Write your solution in this file!
+// var customerName = 'bob'
+// const leastFavoriteCustomer = 'bob is that you?'
 
-function upperCaseCustomerName() {
-    customerName = customerName.toUpperCase();
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer() {
+//     bestCustomer = 'not bob'
+
+// }
+
+// function overwriteBestCustomer(){
+//     bestCustomer = 'maybe bob'
+// }
+
+// function changeLeastFavoriteCustomer(){
+//     leastFavoriteCustomer = 'change please'
+
+// }
+
+var customerName = 'bob'
+const leastFavoriteCustomer = ''
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
 }
 
-function setBestCustomer() {
+function setBestCustomer(){
     bestCustomer = 'not bob'
 
 }
 
 function overwriteBestCustomer(){
     bestCustomer = 'maybe bob'
+
 }
 
 function changeLeastFavoriteCustomer(){
-    leastFavoriteCustomer = 'change please'
+    leastFavoriteCustomer = ''
 
 }
